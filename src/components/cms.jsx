cb84e122f1b99eb74efcac50b58833c05f0abb53
Austine Okongo <austineokongo60@gmail.com>
2022-09-20 21:40:11 +0300

    added blog editor

--- src/components/cms.jsx
+++ src/components/cms.jsx
@@ -1,8 +1,10 @@
+//import axios from 'axios';
 import React, {Component} from 'react';
 import {BiEdit} from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
 import  Modal  from 'react-modal';
 import Zoom from 'react-reveal/Zoom';
+
 
 class ContMan extends Component{
     state={
@@ -16,16 +18,32 @@ class ContMan extends Component{
  closeModal=()=>{
     this.setState({blog:null})
 }
-handleTitle=e=>{
-    this.setState({titleEdit:e.target.value})
+handleEdits=e=>{
+    this.setState({[e.target.name]:e.target.value})
 }
-handleContent=e=>{
-    this.setState({contentEdit:e.target.value})
+
+handleState=(blog)=>{
+return  this.setState({titleEdit:blog.title,contentEdit:blog.content});
+  
 }
+handleSubmit=(e)=>{
+    e.preventDefault()
+     
+const updatedBlog={
+  titleEdit:this.state.titleEdit,
+  contentEdit:this.state.contentEdit
+}
+
+    console.log(updatedBlog);
+   // axios.put('http://localhost:4000/app/admin-blogs/edit/:_id',updatedBlog)
+    
+}
+
+
     render(){
        
         const {blogs}=this.props;
-        const {blog}=this.state;
+        const {blog,titleEdit,contentEdit}=this.state;
         return(
             <div>
              <nav className="navbar bg-light">
@@ -36,30 +54,33 @@ handleContent=e=>{
                
                <div className='content-title'>{blog.title}</div>
                <div className='content-btns'>
-                   <button className='btn btn-success' onClick={()=>this.openModal(blog)}>Edit <BiEdit/></button>
+                   <button className='btn btn-success' onClick={()=>{this.openModal(blog); this.handleState(blog)} }>Edit <BiEdit/></button>
                    <button className='btn btn-danger'>Delete <MdDelete /></button>
                </div>
            </div>)}
            </div>
            {blog && (
-           <Modal isOpen={true}>
+           <Modal isOpen={true} onRequestClose={this.closeModal} ariaHideApp={false}
+           contentLabel="Selected Option">
             <Zoom className='zoom'>
                
-                <form >
+                <form  onSubmit={this.handleSubmit} action="">
+                <label className="form-label">Edit for:{" "}<b>{blog.title}</b></label>
+                <p className='text-muted'>*only edit required field</p>
                 <div>
                     <label htmlFor="up-title" className="form-label">Title</label>
                     <input type="text" className='form-control' id="up-title"
-                     value={blog.title} name='titleEdit' onChange={this.handleTitle} />
+                     name='titleEdit' value={titleEdit}  onChange={this.handleEdits} />
                 </div>
                 <div className="mb-3">
                  <label htmlFor="Textarea1" className="form-label">Blog</label>
-                 <textarea className="form-control" id="Textarea1" rows="7" value={blog.content} 
-                 name='contentEdit' onChange={this.handleContent}/>
+                 <textarea className="form-control" id="Textarea1" rows="7" 
+                 name='contentEdit' value={contentEdit} onChange={this.handleEdits}/>
                 </div>
                 
                 <div className='modal-btns'>
                   <div>
-                  <button className='btn btn-primary' type='submit' onClick={this.closeModal}>Confirm</button> 
+                  <button className='btn btn-primary' type='submit'>Confirm</button> 
                   </div> 
                   <div>
                   <button className='btn btn-danger' onClick={this.closeModal}>Cancel</button>
